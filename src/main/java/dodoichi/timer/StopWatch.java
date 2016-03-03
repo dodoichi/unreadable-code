25ec953f9ae80dad2b35310ef1c4b296c5c5a4dd
yosh <yosh.forrest@gmail.com>
2016-03-04 00:11:27 +0900

    hurt readability of StopWatch to make review effective.
    This commit does not contain any positive change.

--- src/main/java/dodoichi/timer/StopWatch.java
+++ src/main/java/dodoichi/timer/StopWatch.java
@@ -16,19 +16,19 @@ public class StopWatch {
     private Instant i2;
     private Instant i3;
 
-    public StopWatch() {
+    public StopWatch(){
         // initialize d.
         d = Duration.ZERO;
     }
 
     public void start() {
         // ensure this stop watch has not started.
-        if (d.compareTo(Duration.ZERO) > 0) {
+        if (d.compareTo(Duration.ZERO) >0) {
             throw new IllegalStateException("already started");
         }
         // store current instant.
         i1 = Instant.now();
-        System.out.printf("starting at %s%n", i1);
+        System.out.printf("starting at %s%n",i1);
     }
 
     public void stop() {
@@ -61,8 +61,8 @@ public class StopWatch {
         if (i2 == null) {
             throw new IllegalStateException("not suspended");
         }
-        i3 = Instant.now();
-        Duration tmp = Duration.between(i2, i3);
+            i3 = Instant.now();
+            Duration tmp = Duration.between(i2, i3);
         // clear last suspended state.
         i2 = null;
         i3 = Instant.now();
@@ -78,11 +78,11 @@ public class StopWatch {
     }
 
     public Temporal getStartTime() {
-        return i1;
+       return i1;
     }
 
     public Duration getDuration() {
-        return d;
+       return d;
     }
 
 }
