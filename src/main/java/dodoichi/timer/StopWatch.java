1e86c539a09a629286ab2f945ba42681773fe21d
yosh <yosh.forrest@gmail.com>
2016-03-05 18:34:33 +0900

    removed comments, added Javadoc.

--- src/main/java/dodoichi/timer/StopWatch.java
+++ src/main/java/dodoichi/timer/StopWatch.java
@@ -5,7 +5,10 @@ import java.time.Instant;
 import java.time.temporal.Temporal;
 
 /**
- * StopWatch.
+ * StopWatch provides simple API for timings.
+ *
+ * <p>At first, you must call {@link #start()}.
+ * Next, you can call {@link #suspend()} to suspend or {@link #stop()} to end.</p>
  *
  * @author dodoichi
  */
@@ -17,12 +20,16 @@ public class StopWatch {
     private Instant resumedAt;
 
     public StopWatch() {
-        // initialize d.
         totalDuration = Duration.ZERO;
     }
 
+    /**
+     * Start the stopwatch.
+     * If this stopwatch has been started, {@link IllegalStateException} will be thrown.
+     *
+     * @throws IllegalStateException if the stopwatch has been started.
+     */
     public void start() {
-        // ensure this stop watch has not started.
         if (totalDuration.compareTo(Duration.ZERO) > 0) {
             throw new IllegalStateException("already started");
         }
@@ -31,44 +38,65 @@ public class StopWatch {
         System.out.printf("starting at %s%n",startedAt);
     }
 
+    /**
+     * Stop the stopwatch.
+     * If it has not started, {@link IllegalStateException} will be thrown.
+     */
     public void stop() {
-        // if not started throw exception.
         if (startedAt == null) {
             throw new IllegalStateException("not started");
         }
+
         Instant i4 = Instant.now();
-        Duration tmp = resumedAt != null ? Duration.between(resumedAt, i4) : Duration.between(startedAt, i4);
-        totalDuration = totalDuration.plus(tmp);
+        Duration lastDuration;
+        if (resumedAt != null) {
+            lastDuration = Duration.between(resumedAt, i4);
+        } else {
+            lastDuration = Duration.between(startedAt, i4);
+        }
+        totalDuration = totalDuration.plus(lastDuration);
         System.out.printf("stopped at %s. total duration is %s%n", i4, totalDuration);
     }
 
+    /**
+     * Suspend the stopwatch.
+     * if it has been already suspended, {@link IllegalStateException} will be thrown.
+     *
+     * @throws IllegalStateException if the stopwatch has not been started, or been suspended.
+     */
     public void suspend() {
         if (startedAt == null) {
             throw new IllegalStateException("not started");
         }
-        // if already suspended throw exception.
         if (suspendedAt != null) {
             throw new IllegalStateException("already suspended.");
         }
-        // suspend stop watch. save duration since start timing.
         suspendedAt = Instant.now();
         totalDuration = totalDuration.plus(Duration.between(startedAt, suspendedAt));
         System.out.printf("suspended at %s. current duration is %s%n", suspendedAt, totalDuration);
     }
 
+    /**
+     * Resume the stopwatch from suspended state. if not suspended throw exception.
+     * Suspended state will be cleared after resumed.
+     *
+     * @throws IllegalStateException if the stopwatch has not been suspended.
+     */
     public void resume() {
-        // resume suspended state. if not suspended throw exception.
         if (suspendedAt == null) {
             throw new IllegalStateException("not suspended");
         }
+
         resumedAt = Instant.now();
         Duration suspendedDuration = Duration.between(suspendedAt, resumedAt);
-        // clear last suspended state.
         suspendedAt = null;
         resumedAt = Instant.now();
         System.out.printf("during %s from suspended. now resuming at %s%n", suspendedDuration, resumedAt);
     }
 
+    /**
+     * Reset the stopwatch.
+     */
     public void reset() {
         // reset all state.
         startedAt = null;
