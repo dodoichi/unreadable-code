8b03f718b35dab77ce3e85bb7d3e426fff647fad
yosh <yosh.forrest@gmail.com>
2016-03-05 16:46:40 +0900

    renamed meaningless names
    * avoiding generic name
    * choosing concrete name
    * adding extra information to names

--- src/main/java/dodoichi/timer/StopWatch.java
+++ src/main/java/dodoichi/timer/StopWatch.java
@@ -11,78 +11,78 @@ import java.time.temporal.Temporal;
  */
 public class StopWatch {
 
-    private Duration d;
-    private Instant i1;
-    private Instant i2;
-    private Instant i3;
+    private Duration totalDuration;
+    private Instant startedAt;
+    private Instant suspendedAt;
+    private Instant resumedAt;
 
     public StopWatch() {
         // initialize d.
-        d = Duration.ZERO;
+        totalDuration = Duration.ZERO;
     }
 
     public void start() {
         // ensure this stop watch has not started.
-        if (d.compareTo(Duration.ZERO) > 0) {
+        if (totalDuration.compareTo(Duration.ZERO) > 0) {
             throw new IllegalStateException("already started");
         }
         // store current instant.
-        i1 = Instant.now();
-        System.out.printf("starting at %s%n",i1);
+        startedAt = Instant.now();
+        System.out.printf("starting at %s%n",startedAt);
     }
 
     public void stop() {
         // if not started throw exception.
-        if (i1 == null) {
+        if (startedAt == null) {
             throw new IllegalStateException("not started");
         }
         Instant i4 = Instant.now();
-        Duration tmp = i3 != null ? Duration.between(i3, i4) : Duration.between(i1, i4);
-        d = d.plus(tmp);
-        System.out.printf("stopped at %s. total duration is %s%n", i4, d);
+        Duration tmp = resumedAt != null ? Duration.between(resumedAt, i4) : Duration.between(startedAt, i4);
+        totalDuration = totalDuration.plus(tmp);
+        System.out.printf("stopped at %s. total duration is %s%n", i4, totalDuration);
     }
 
     public void suspend() {
-        if (i1 == null) {
+        if (startedAt == null) {
             throw new IllegalStateException("not started");
         }
         // if already suspended throw exception.
-        if (i2 != null) {
+        if (suspendedAt != null) {
             throw new IllegalStateException("already suspended.");
         }
         // suspend stop watch. save duration since start timing.
-        i2 = Instant.now();
-        d = d.plus(Duration.between(i1, i2));
-        System.out.printf("suspended at %s. current duration is %s%n", i2, d);
+        suspendedAt = Instant.now();
+        totalDuration = totalDuration.plus(Duration.between(startedAt, suspendedAt));
+        System.out.printf("suspended at %s. current duration is %s%n", suspendedAt, totalDuration);
     }
 
     public void resume() {
         // resume suspended state. if not suspended throw exception.
-        if (i2 == null) {
+        if (suspendedAt == null) {
             throw new IllegalStateException("not suspended");
         }
-        i3 = Instant.now();
-        Duration tmp = Duration.between(i2, i3);
+        resumedAt = Instant.now();
+        Duration suspendedDuration = Duration.between(suspendedAt, resumedAt);
         // clear last suspended state.
-        i2 = null;
-        i3 = Instant.now();
-        System.out.printf("during %s from suspended. now resuming at %s%n", tmp, i3);
+        suspendedAt = null;
+        resumedAt = Instant.now();
+        System.out.printf("during %s from suspended. now resuming at %s%n", suspendedDuration, resumedAt);
     }
 
     public void reset() {
         // reset all state.
-        i1 = null;
-        i2 = null;
-        i3 = null;
-        d = Duration.ZERO;
+        startedAt = null;
+        suspendedAt = null;
+        resumedAt = null;
+        totalDuration = Duration.ZERO;
     }
 
     public Temporal getStartTime() {
-        return i1;
+        return startedAt;
     }
 
     public Duration getDuration() {
-        return d;
+        return totalDuration;
     }
 
 }
